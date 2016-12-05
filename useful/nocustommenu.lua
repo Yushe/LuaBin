942ccd81f1c5f8f37ef2ed865123aaf2c22cf024
Yushe <Yushe@users.noreply.github.com>
2016-12-05 15:58:16 +0000

    Add nocustommenu.lua

---
+++ useful/nocustommenu.lua
@@ -0,0 +1,6 @@
+function gui.IsGameUIVisible() return false end
+
+--Disables annoying custom ESC menus on gmod servers
+--Credit to Kevlon from facepunch
+
+--Scriptfodder was a mistake. -lNloruzenchi

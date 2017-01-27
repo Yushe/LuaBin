c2699d8b002a0c227ad6c52707c3e7b337bdf1e8
Yushe <Yushe@users.noreply.github.com>
2017-01-27 09:23:14 +0000

    Update checkach.lua

--- tests/checkach.lua
+++ tests/checkach.lua
@@ -1,4 +1,5 @@
 function GM:PlayerSpawn
-if ply:achievements.IsAchieved( 7 ) = false
+if ply:achievements.IsAchieved( 7 ) = false then
    ply:ChatPrint( ply:Nick() .. " has not got" .. achievements.IsAchieved( 7 ) )
+   end
 end
